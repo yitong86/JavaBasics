aaee4e20364712b5e79e58a4f1556d2dab65d28d
yitong86 <YOUR_EMAIL_ADDRESS>
2022-01-06 10:50:04 -0500

    Full Name

--- src/com/sophia/Main.java
+++ src/com/sophia/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String firstName = "yitong";
+        String lastName = "Jiang";
+        String fullName = "yitong Jiang";
+        System.out.println(fullName);
+        creatFullName("yitong","jiang");
+        System.out.println(firstName + " " + lastName);
+    }
+    public static String creatFullName(String firstName,String lastName) {
+         return firstName + " " + lastName;
     }
 }
